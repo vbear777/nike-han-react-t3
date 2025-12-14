c624774e9c4bebadedff54e99109e93fa6670ec2
HanVbear <ryoitenkaii@gmail.com>
2025-12-14 22:28:35 +0700

    add super quality product section

--- src/sections/SuperQuality.jsx
+++ src/sections/SuperQuality.jsx
@@ -1,8 +1,45 @@
+import { ArrowRight } from "lucide-react";
+import Button from "../components/Button";
+import { shoe8 } from "../assets/images";
+
 const SuperQuality = () => {
     return (
-        <div>
-            SuperQuality
-        </div>
+        <section
+            id="about-us"
+            className="flex justify-between items-center max-lg:flex-col gap-10 w-full max-container"
+        >
+            <div className="flex flex-1 flex-col">
+                <h2 className="font-palanquin text-4xl capitalize font-bold lg:max-w-lg">
+                    We Provide You
+                    <span className="text-red"> Super</span>
+                    <span className="text-red"> Quality</span> Shoes
+                </h2>
+                <p className="mt-4 lg:max-w-lg info-text ">
+                    Featuring a modern design and premium quality, Nike Super Quality offers maximum comfort and a sporty look. 
+                    Made from carefully selected materials with neat and precise detailing, this product provides excellent 
+                    durability and comfort for various activities, from sports to everyday casual wear. With strong stitching, 
+                    soft yet durable materials, and a design that follows the latest trends.
+                </p>
+                <p className="mt-6 lg:max-w-lg info-text">
+                    Nike Super Quality is the perfect choice for those who value both performance and style in one product. 
+                    Suitable for active lifestyles, traveling, or enhancing your daily appearance to stay stylish and confident.
+                </p>
+                <div className="mt-11">
+                    <Button label="View details"/>
+                </div>
+            </div>
+
+            <div className="flex-1 flex justify-center items-center">
+                <img 
+                    src={shoe8} 
+                    alt="shoe8" 
+                    width={570}
+                    height={522}
+                    className="object-contain"
+                />
+            </div>
+
+        </section>
     )
 }
 
