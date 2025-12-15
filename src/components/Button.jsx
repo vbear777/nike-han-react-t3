55f23d7d3df10422f8eac1537ba82342ba6ddd73
HanVbear <ryoitenkaii@gmail.com>
2025-12-15 11:56:44 +0700

    add special offer section

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,24 +1,24 @@
-const Button = ({ label, icon: Icon }) => {
+const Button = ({ label, icon: Icon, backgroundColor, textColor, borderColor }) => {
     return (
         <button 
-            className="relative overflow-hidden
+            className={`relative overflow-hidden
                         flex justify-center items-center gap-2
                         px-7 py-4
                         font-montserrat text-lg leading-none
                         rounded-full
-                        text-white
-
-                        bg-red
-                        border border-blood
-
+                        border
                         before:content-['']
                         before:absolute before:inset-0
                         before:bg-blood
                         before:origin-left
                         before:scale-x-0
                         before:transition-transform before:duration-300 before:ease-out
-                        hover:before:scale-x-100"
-        >
+                        hover:before:scale-x-100
+                        ${backgroundColor ? 
+                            ` ${backgroundColor} ${textColor} ${borderColor}` : 
+                            "text-white bg-red border-blood"
+                        }            
+            `}>
             <span className="relative z-10">{label}</span>
 
             <span className="relative z-10">{Icon && <Icon className="w-5 h-5" />}</span>
--- src/sections/SpecialOffer.jsx
+++ src/sections/SpecialOffer.jsx
@@ -1,8 +1,47 @@
+import { offer } from "../assets/images";
+import { ArrowRight } from "lucide-react";
+import Button from "../components/Button";
+import { BookSearch } from "lucide-react";
+
 const SpecialOffer = () => {
     return (
-        <div>
-            SpecialOffer
-        </div>
+        <section className="flex justify-wrap items-center max-xl:flex-col-reverse gap-10 ">
+            <div className="flex-1">
+                <img 
+                    src={offer} 
+                    width={773}
+                    height={687}
+                    className="object-contain w-full"
+                />
+            </div>
+
+            <div className="flex flex-1 flex-col">
+                <h2 className="font-palanquin text-4xl capitalize font-bold lg:max-w-lg">
+                    <span className="text-red">Special</span> Offer
+                </h2>
+                <p className="mt-4 lg:max-w-lg info-text ">
+                    Upgrade your performance with Nike’s latest collection. Enjoy exclusive deals on premium footwear and 
+                    apparel designed for comfort, style, and durability. Limited time only — don’t miss out.
+                </p>
+                <p className="mt-6 lg:max-w-lg info-text">
+                    Push your limits with Nike. Get exclusive discounts on selected shoes and apparel crafted with innovative 
+                    technology for maximum performance. Shop now and elevate your game before the offer ends.
+                </p>
+                <div className="mt-11 flex flex-wrap gap-4 ">
+                    <Button 
+                        label="Shop now" 
+                        icon={ArrowRight} 
+                    />
+                    <Button 
+                        label="learn more" 
+                        backgroundColor="bg-white" 
+                        borderColor="border-blood" 
+                        textColor="text-black font-bold"
+                        icon={BookSearch}
+                    />
+                </div>
+            </div>
+        </section>
     )
 }
 
